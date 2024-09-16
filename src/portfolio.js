855ab5611ae1de619ac1d6e3bf64a7b832975390
RishabhhG <hydrarishabh9@gmail.com>
2024-09-17 02:53:42 +0530

    Final_commit

---
+++ src/portfolio.js
@@ -0,0 +1,579 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: 'UA-174238252-2'
+};
+
+//Home Page
+const greeting = {
+  title: 'Hello ',
+  title2: 'Rishabh',
+  logo_name: 'R_Dev.js()',
+  nickname: 'Pboss',
+  full_name: 'Rishabh Gupta',
+  subTitle: [
+    'Full Stack Developer',
+    'Cloud Computing',
+    'Graphic Designer'
+  ],
+  resumeLink:
+    'https://drive.google.com/file/d/1SUxhzC78pgW2Ccotb2unGU2i0TUWm789/view',
+  mail: 'mailto:rishabhh0807@gmail.com'
+};
+
+const socialMediaLinks = {
+  /* Social Media Link */
+  github: 'https://github.com/RishabhhG',
+  linkedin: 'https://www.linkedin.com/in/rishabh-guptaaa/',
+  gmail: 'rishabhh0807@gmail.com',
+  instagram: 'https://www.instagram.com/rishabhh__.__?igsh=MTRwaTdybnh2eXdzag=='
+};
+const skillprogress = {
+  skillprogresses: [
+    {
+      name: 'Front End Developer',
+      done: '75',
+      icon: 'mdi:web'
+    },
+    {
+      name: 'Back End Developer',
+      done: '70',
+      icon: 'whh:website'
+    },
+    {
+      name: 'Machine Learning',
+      done: '80',
+      icon: 'eos-icons:machine-learning'
+    },
+    {
+      name: 'Deep Learning',
+      done: '60',
+      icon: 'carbon:machine-learning-model'
+    },
+    {
+      name: 'Data Structures',
+      done: '65',
+      icon: 'carbon:data-structured'
+    },
+    {
+      name: 'Graphic Designing',
+      done: '75',
+      icon: 'fluent:design-ideas-16-filled'
+    },
+    {
+      name: 'Video Editing',
+      done: '65',
+      icon: 'bi:camera-video-fill'
+    },
+    {
+      name: 'App Development',
+      done: '55',
+      icon: 'ant-design:android-filled'
+    },
+    {
+      name: 'Internet of Things',
+      done: '60',
+      icon: 'eos-icons:iot'
+    },
+    {
+      name: 'Game Development',
+      done: '40',
+      icon: 'entypo:game-controller'
+    },
+    {
+      name: 'Augmented Reality',
+      done: '40',
+      icon: 'ic:twotone-view-in-ar'
+    }
+  ]
+};
+const skills = {
+  data: [
+    {
+      title: 'MERN Stack Development',
+      fileName: 'FullStackImg',
+      skills: [
+        '🌐 Develop highly interactive Front end / User Interfaces for your web and mobile applications',
+        '🌐 Building responsive website front end using ReactJS',
+        '🌐 Designing and managing databases using MySQL, MongoDB, and other modern databases',
+        '🌐 Creating application backend in Node, Express',
+        '🌐 Integration of third party services such as Firebase/ AWS / Digital Ocean'
+      ],
+      softwareSkills: [
+        {
+          skillName: 'HTML5',
+          fontAwesomeClassname: 'simple-icons:html5',
+          style: {
+            color: '#E34F26'
+          }
+        },
+        {
+          skillName: 'CSS3',
+          fontAwesomeClassname: 'fa-css3',
+          style: {
+            color: '#1572B6'
+          }
+        },
+        {
+          skillName: 'JavaScript',
+          fontAwesomeClassname: 'simple-icons:javascript',
+          style: {
+            backgroundColor: '#FFFFFF',
+            color: '#F7DF1E'
+          }
+        },
+
+        {
+          skillName: 'ReactJS',
+          fontAwesomeClassname: 'simple-icons:react',
+          style: {
+            color: '#61DAFB'
+          }
+        },
+        {
+          skillName: 'NodeJS',
+          fontAwesomeClassname: 'simple-icons:node-dot-js',
+          style: {
+            color: '#339933'
+          }
+        },
+        {
+          skillName: 'NPM',
+          fontAwesomeClassname: 'simple-icons:npm',
+          style: {
+            color: '#CB3837'
+          }
+        },
+        {
+          skillName: 'MongoDB',
+          fontAwesomeClassname: 'simple-icons:mongodb',
+          style: {
+            color: '#439743'
+          }
+        },
+        {
+          skillName: 'GraphQL',
+          fontAwesomeClassname: 'simple-icons:graphql',
+          style: {
+            color: '#DE33A6'
+          }
+        },
+        {
+          skillName: 'MySQL',
+          fontAwesomeClassname: 'simple-icons:mysql',
+          style: {
+            color: '#4479A1'
+          }
+        },
+        {
+          skillName: 'SASS',
+          fontAwesomeClassname: 'simple-icons:sass',
+          style: {
+            color: '#c69'
+          }
+        },
+        {
+          skillName: 'Socket.io',
+          fontAwesomeClassname: 'simple-icons:socketdotio',
+          style: {
+            color: '#1D9723'
+          }
+        },
+        {
+          skillName: 'jQuery',
+          fontAwesomeClassname: 'simple-icons:jquery',
+          style: {
+            color: '#0865A6'
+          }
+        },
+        {
+          skillName: 'Tailwind',
+          fontAwesomeClassname: 'simple-icons:tailwindcss',
+          style: {
+            color: '#3fb6d3'
+          }
+        },
+        {
+          skillName: 'Bootstrap',
+          fontAwesomeClassname: 'simple-icons:bootstrap',
+          style: {
+            color: '#59407f'
+          }
+        },
+        {
+          skillName: 'Git',
+          fontAwesomeClassname: 'simple-icons:git',
+          style: {
+            color: '#E94E32'
+          }
+        },
+        {
+          skillName: 'Github',
+          fontAwesomeClassname: 'simple-icons:github',
+          style: {
+            color: '#101010'
+          }
+        },
+        {
+          skillName: 'C++',
+          fontAwesomeClassname: 'simple-icons:cplusplus',
+          style: {
+            color: '#005494'
+          }
+        },
+        
+        {
+          skillName: 'Express',
+          fontAwesomeClassname: 'simple-icons:express',
+          style: {
+            color: '#E94E32'
+          }
+        },
+        {
+          skillName: 'Firebase',
+          fontAwesomeClassname: 'simple-icons:firebase',
+          style: {
+            color: '#f58220'
+          }
+        },
+      ]
+    },
+    
+    {
+      title: 'Technical Softwares',
+      fileName: 'CloudInfraImg',
+      skills: [
+        '⚡ Experience working on multiple hosting platform',
+        '⚡ Experience hosting and Developing webpages and projects',
+        '⚡ Experience with Continuous Integration'
+      ],
+      softwareSkills: [
+        {
+          skillName: 'AWS',
+          fontAwesomeClassname: 'simple-icons:amazonaws',
+          style: {
+            color: '#FF9900'
+          }
+        },
+        {
+          skillName: 'Netlify',
+          fontAwesomeClassname: 'simple-icons:netlify',
+          style: {
+            color: '#38AFBB'
+          }
+        },
+        {
+          skillName: 'Heroku',
+          fontAwesomeClassname: 'simple-icons:heroku',
+          style: {
+            color: '#6863A6'
+          }
+        },
+        {
+          skillName: 'Firebase',
+          fontAwesomeClassname: 'simple-icons:firebase',
+          style: {
+            color: '#FFCA28'
+          }
+        },
+
+        {
+          skillName: 'MongoDB',
+          fontAwesomeClassname: 'simple-icons:mongodb',
+          style: {
+            color: '#47A248'
+          }
+        },
+        {
+          skillName: 'Unity',
+          fontAwesomeClassname: 'simple-icons:unity',
+          style: {
+            color: '#47A998'
+          }
+        }
+      ]
+    }
+  ]
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: 'Vellore Institute of Technology, Bhopal',
+      subtitle: 'Bachelor in Computer Science and Engineering',
+      logo_path: 'vellore-institute-of-technology-vit-university-logo.png',
+      alt_name: 'NITH',
+      duration: '2021 - Present',
+      descriptions: [
+        "⚡ I'm currently pursuing my bachelors in Computer Science and Engineering.",
+        '⚡ I have studied core subjects like Data Structures, DBMS, etc.',
+        '⚡ I have also completed various online courses for Backend , Frontend, Cloud Computing etc.',
+        "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. "
+      ],
+    },
+    {
+      title: 'D.A.V Public School',
+      subtitle: 'High School and Intermediate',
+      logo_path: 'school.jpg',
+      alt_name: 'VBPS',
+      duration: '2018 - 2021',
+      descriptions: [
+        '⚡ I have completed my High school and Intermediate from this School.',
+        '⚡ I have participated many inter school compition based on science and research.'
+      ],
+    }
+  ]
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: 'AWS Cloud Practitioner',
+      subtitle: 'AWS',
+      logo_path: 'courcera.png',
+      img_path: 'AWS-Certified-Cloud-Practitioner_badge.634f8a21af2e0e956ed8905a72366146ba22b74c.png',
+      alt_name: 'Stanford University',
+      color: '#0B5394',
+      color_code: '#0096FF',
+      discription:
+        'The AWS Certified Cloud Practitioner validates foundational, high-level understanding of AWS.'
+    },
+
+    {
+      title: 'NPTEL Cloud Computing',
+      subtitle: 'NPTEL',
+      logo_path: 'NPTEL-A-complete-Guide.png',
+      img_path: 'NPTEL-A-complete-Guide.png',
+      alt_name: 'DeepLearning.ai',
+      color_code: 'rgb(0, 150, 255)',
+      discription:
+        'An online non-credit course authorized by NPTEL and offered by GOVT. Of India'
+    },
+    {
+      title: 'ReactJs Course',
+      subtitle: 'Udemy',
+      logo_path: 'udemy.png',
+      img_path: 'webd.png',
+      alt_name: 'Workshop',
+      color_code: '#676FA3',
+      discription: 'The complete ReactJS Course- Basic to Advance'
+    },
+
+    {
+      title: 'Web Development Intermediate Course',
+      subtitle: 'Guide to Modern Web Development',
+      logo_path: 'gssoc.png',
+      img_path: 'web3.png',
+      alt_name: 'GIRLSCRIPT.TECH',
+      color_code: '#FF5733',
+      discription:
+        'Web Development Intermediate Course as a part of Education Outreach Program '
+    },
+    {
+      title: 'CSS Skill Assessment',
+      subtitle: 'HackerRank',
+      logo_path: 'HackerRank.png',
+      img_path: 'webd2.png',
+      alt_name: 'HackerRank',
+      color_code: '#FF5733',
+      discription:
+        'Web Development Intermediate Course '
+    }
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ]
+};
+//aboutPage
+const about = {
+  title: 'About',
+  discriptionLine: 'Full Stack Developer, Tech enthusiast.',
+  extraLine: ' Always in my learning phase.',
+  discription:
+    ' Creative and Tech-savvy Web Developer intern dedicated to develop, contribute and optimize Websites and Web app which are interactive and userfriendly. Team player with an eye for detail. Judicious and creative when crafting effective websites, apps and platforms to propel competitive advantage and revenue growth. Technically proficient and leverage analytical problem solver with calm and focused demeanor.'
+};
+// Experience Page
+const experience = {
+  title: 'Experience',
+  subtitle: 'Work, Internship and Volunteership',
+  description:
+    "I've mostly done projects on Web Development and I am actively looking for collaboration. I love exchanging words on latest technology to share my knowledge with others.",
+  header_image_path: 'experience.svg',
+  sections: [
+    {
+      title: 'Work Experience',
+      experiences: [
+        {
+          title: 'Community and Database Management (Intern)',
+          company: 'Sunstone',
+          company_url: 'https://sunstone.in/',
+          logo_path: 'download.jpeg',
+          duration: 'Mar-2022 - Jun 2022',
+          location: 'Gurugram',
+          description: `Worked as a Database Management intern at Sunstone was a great experience to learn new technologies and work on real world projects.
+          `,
+          color: '#0071C5'
+        },
+        
+      ]
+    }
+  ]
+};
+
+// Projects Page
+const projectsHeader = {
+  title: 'Projects',
+  description:
+    'My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects,React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.',
+  avatar_image_path: 'projects_image.svg'
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: 'Contact Me',
+    profile_image_path: '360_F_313404541_e9YZ3pht6oEEkMXuhxTboqXA2B2ShNnC.jpg',
+
+    description:
+      'You can contact me at the places mentioned below. I will try to get back to you as fast as I can. '
+  },
+  blogSection: {
+    title: 'Blogs',
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others. I write on Twitter.",
+    twiter_link: '',
+    medium_link: '',
+    avatar_image_path: 'blogs_image.svg'
+  }
+};
+
+const projects = {
+  data: [
+    {
+      id: '0',
+      name: 'TravelMingle',
+      url: 'https://github.com/RishabhhG/TravelMingle',
+      img_url: 'world-tourism-day-asia-summer-travel-vacation-banner-generative-ai-illustration_145713-5937.avif',
+      description:
+        'The AI Trip Planner is a web application designed to create personalized travel itineraries based on user preferences. It leverages Google Gemini AI and various APIs to provide tailored travel experiences. Integrated Gemini AI v1.5 to generate trip plans and utilized Shadcn/UI to design a responsive interface and Integrated Google Photo API to display place images and added React Google Autocomplete.',
+      languages: [
+        {
+          name: 'MongoDb',
+          iconifyClass: "logos-mongodb"
+        },
+        {
+          name: 'HTML5',
+          iconifyClass: 'vscode-icons:file-type-html'
+        },
+        {
+          name: 'CSS3',
+          iconifyClass: 'vscode-icons:file-type-css'
+        },
+        {
+          name: 'React',
+          iconifyClass: 'logos-react'
+        },
+        {
+          name: 'NodeJS',
+          iconifyClass: 'logos-nodejs'
+        }
+      ]
+    },
+
+    {
+      id: '2',
+      name: 'KnowlegdeHub',
+      url: 'https://github.com/RishabhhG/KnowledgeHub',
+      img_url: 'students-using-e-learning-platform-video-laptop-graduation-cap-online-education-platform-e-learning-platform-online-teaching-concept_335657-795.png',
+      description:
+        'KnowledgeHub is an advanced educational technology platform engineered with the MERN stack (MongoDB, ExpressJS, ReactJS, NodeJS). It is designed to revolutionize online learning by providing a seamless, engaging, and accessible experience for students, while empowering instructors to share their expertise and connect with a global audience.',
+      languages: [
+        {
+          name: 'NodeJS',
+          iconifyClass: 'logos-nodejs'
+        },
+        {
+          name: 'HTML5',
+          iconifyClass: 'vscode-icons:file-type-html'
+        },
+        {
+          name: 'CSS3',
+          iconifyClass: 'vscode-icons:file-type-css'
+        },
+        {
+          name: 'JavaScript',
+          iconifyClass: 'logos-javascript'
+        },
+        {
+          name: 'React',
+          iconifyClass: 'logos-react'
+        },
+        {
+          name: "Firestore",
+          iconifyClass: "logos:firebase"
+        },
+        {
+          name: "Gemini API",
+          iconifyClass: "mdi:api"
+        }
+        
+        
+      ]
+    },
+    {
+      id: '3',
+      name: 'MediChat',
+      url: 'https://github.com/RishabhhG/medichat',
+      img_url: 'istockphoto-1574636171-612x612.jpg',
+      description:
+        'MediChat is a real-time chat application designed to connect patients and healthcare professionals. Built using the MERN stack (MongoDB, Express.js, React, and Node.js), it enables secure and seamless communication between users. The app features an intuitive interface for conversations, allows appointment scheduling, and integrates notifications to streamline communication in healthcare environments.',
+      languages: [
+        {
+          name: 'MongoDb',
+          iconifyClass: "logos-mongodb"
+        },
+        {
+          name: 'HTML5',
+          iconifyClass: 'vscode-icons:file-type-html'
+        },
+        {
+          name: 'CSS3',
+          iconifyClass: 'vscode-icons:file-type-css'
+        },
+        {
+          name: 'React',
+          iconifyClass: 'logos-react'
+        },
+        {
+          name: 'NodeJS',
+          iconifyClass: 'logos-nodejs'
+        }
+      
+      ]
+    },
+  ]
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  about,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+  skillprogress
+};
